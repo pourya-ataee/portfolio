9895f34c8f3ac5358dea3b73a42fb211de759d86
Pourya <p.ataee.dev@gmail.com>
2022-11-26 21:36:08 +0330

    build[view]: add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import Error from './components/error/Error';
+import Home from './components/main/Home';
+import About from './components/about/About';
+import Layout from './components/main/Layout';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Route>
+    </Routes>
   );
 }
 
